8be0c0c80403a48e2d7139dc22a554eaed5b966a
HaQadosch <haqadosch@gmail.com>
2019-04-04 12:59:07 +0100

    First React Hook
    Button Click event handler

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 const Button = props => {
-  const [counter, setCounter] = useState();
-  return <button>Test</button>;
+  const [counter, setCounter] = useState(0);
+  const incCounter = _ => setCounter(counter + 1);
+  return <button onClick={incCounter}>{counter}</button>;
 };
 
 const rootElement = document.getElementById("root");
