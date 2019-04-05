f6c0a9ec7e84ac1eabc8e76bb97fa809e335c779
HaQadosch Berraka <haqadosch@pm.me>
2019-04-05 19:26:02 +0100

    GitHub CardsGame: sample data for the CardList component.

--- github-cardsgame/src/Components/Card.js
+++ github-cardsgame/src/Components/Card.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 import '../Style/Card.css'
 export class Card extends Component {
   render () {
+    const { profile: { name, avatar_url: avatarURL, company } } = this.props
     return (
       <section className='h-card' >
         <figure>
-          <img className='u-photo' src='https://placehold.it/75' alt='profile of name' />
-          <figcaption className='p-name'>name</figcaption>
+          <img className='u-photo' src={avatarURL} alt={`profile of ${name}`} />
+          <figcaption className='p-name'>{name}</figcaption>
         </figure>
         <div>
-          <p className='p-name'>Name</p>
-          <p className='p-org'>Organisation</p>
+          <p className='p-name'>{name}</p>
+          <p className='p-org'>{company}</p>
         </div>
       </section>
     )
--- github-cardsgame/src/Components/CardList.js
+++ github-cardsgame/src/Components/CardList.js
@@ -2,8 +2,14 @@ import React from 'react'
 import { Card } from './Card'
 import '../Style/CardList.css'
 
+const testData = [
+  { name: 'Dan Abramov', avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4', company: '@facebook' },
+  { name: 'Sophie Alpert', avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4', company: 'Humu' },
+  { name: 'Sebastian Markbåge', avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4', company: 'Facebook' }
+]
+
 export const CardList = props => (
   <ul>
-    <li className='jey'><Card /></li>
+    {testData.map((profile, index) => <li key={index} ><Card profile={profile} /></li>)}
   </ul>
 )
