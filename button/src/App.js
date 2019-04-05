f048e198f78c05c7dcb3540992416ae0b57b16d2
HaQadosch Berraka <haqadosch@pm.me>
2019-04-05 09:28:10 +0100

    Button: use Fragment

--- button/src/App.js
+++ button/src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
 import './App.css'
 
-const Button = ({ action }) => {
-  return <button onClick={action} > +1 </button>
+const Button = ({ action, amount }) => {
+  const handleClick = _ => action(amount)
+  return <button onClick={handleClick} > +{amount} </button>
 }
 
 const Display = ({ message }) => <div>{message}</div>
 
 const App = props => {
   const [counter, setCounter] = useState(0)
-  const incCounter = _ => setCounter(counter + 1)
+  const incCounter = amount => setCounter(counter + amount)
   console.log(incCounter)
 
   return (
     <>
-      <Button action={incCounter} />
+      <Button action={incCounter} amount={5} />
+      <Button action={incCounter} amount={10} />
+      <Button action={incCounter} amount={20} />
+      <Button action={incCounter} amount={100} />
       <Display message={counter} />
     </>
   )
