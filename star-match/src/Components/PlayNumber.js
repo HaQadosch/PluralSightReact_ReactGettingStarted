fbb5bd46e357d40b2b8ceb9b122f16605893afe6
HaQadosch Berraka <haqadosch@pm.me>
2019-04-06 16:53:30 +0100

    star-match: new states and update ui accordingly.

--- star-match/src/Components/PlayNumber.js
+++ star-match/src/Components/PlayNumber.js
@@ -1,3 +1,4 @@
 import React from 'react'
+import { colors } from '../Utils/utils'
 
-export const PlayNumber = ({ btnId, ...props }) => <button className='number' onClick={_ => console.log({ btnId })} >{btnId}</button>
+export const PlayNumber = ({ btnId, status, ...props }) => <button className='number' style={{ background: colors[status] }} onClick={_ => console.log({ btnId })} >{btnId}</button>
--- star-match/src/Components/StarMatch.js
+++ star-match/src/Components/StarMatch.js
@@ -5,6 +5,21 @@ import { StarsDisplay } from './StarsDisplay'
 
 export const StarMatch = () => {
   const [nbrStars, setNbrStars] = useState(utils.random(1, 9))
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
+  const [candidateNums, setCandidateNums] = useState([])
+
+  const candidatesAreWrong = nbrStars <= utils.sum(candidateNums)
+
+  const numberStatus = number => {
+    let status = 'available'
+    if (!availableNums.includes(number)) {
+      status = 'used'
+    } else if (candidateNums.includes(number)) {
+      status = candidatesAreWrong ? 'wrong' : 'candidate'
+    }
+    return status
+  }
+
   return (
     <div className='game'>
       <div className='help'>
@@ -15,7 +30,7 @@ export const StarMatch = () => {
           <StarsDisplay count={nbrStars} />
         </div>
         <div className='right'>
-          {utils.range(1, 9).map(btnId => <PlayNumber key={btnId} btnId={btnId} />)}
+          {utils.range(1, 9).map(btnId => <PlayNumber key={btnId} btnId={btnId} status={numberStatus(btnId)} />)}
         </div>
       </div>
       <div className='timer'>Time Remaining: 10</div>
